ca3431582a7b67bbd488dee510c595a1a3c1e6bc
Tymon Langowski <t.langowski.012@studms.ug.edu.pl>
2025-03-19 16:57:49 +0100

    lol

---
+++ petlezad4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petlezad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n, k;
+            Console.Write("podaj liczbę naturalną n dodatnią: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("podaj liczbę naturalną dodatnią k: ");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= k; j++)
+                    Console.Write("*"); 
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey(true); 
+    }
+    }
+}
