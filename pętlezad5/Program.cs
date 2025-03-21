1d24191d1d3fa97a6bfae409195f6e3dd0c87c44
Tymon Langowski <t.langowski.012@studms.ug.edu.pl>
2025-03-21 13:38:18 +0100

    a

---
+++ pętlezad5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pętlezad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.Write("podaj liczbę naturalną n większą od 1: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.WriteLine("");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
