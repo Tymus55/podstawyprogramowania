40f3de1279902b71ebfe164e1a98004a9d919ba9
Tymon Langowski <t.langowski.012@studms.ug.edu.pl>
2025-03-31 13:47:41 +0200

    kkkk

---
+++ forzad9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forzad9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= 10; j++)
+                {
+                    Console.Write("{0} ", i * j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ pętlaforzad8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pętlaforzad8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rok1, rok2, i;
+            Console.WriteLine("Podaj pierwszy rok: ");
+            rok1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugi rok: ");
+            rok2 = Convert.ToInt32(Console.ReadLine());
+            for (i = rok1; i <= rok2; i++)
+            {
+                if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
