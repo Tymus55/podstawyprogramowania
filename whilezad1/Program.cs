a5fd8d39ddb3b35c9b3a3da44ad1745531979efd
Tymon Langowski <t.langowski.012@studms.ug.edu.pl>
2025-04-04 13:36:35 +0200

    a

---
+++ whilezad1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whilezad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, i;
+            Console.Write("podaj a: ");
+                a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("podaj b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            i = a;
+            while (i <= b)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ whilezad6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whilezad6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, an, i;
+            Console.Write("podaj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            i = 0;
+            while (++i <= n)
+            {
+                an = 2 * i - 1;
+                Console.WriteLine("a({0})={1}",i, an);
+
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
