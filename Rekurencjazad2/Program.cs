4bc12c687c0173f62915d4058139c61d421a8714
Tymon Langowski <t.langowski.012@studms.ug.edu.pl>
2025-05-15 20:02:55 +0200

    hhyhhh

---
+++ Rekurencjazad2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekurencjazad2
+{
+    using System;
+
+    namespace UczniowieApp
+    {
+        struct Uczen
+        {
+            public string imie;
+            public string nazwisko;
+            public int[] oceny;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Podaj liczbę uczniów: ");
+                int liczbaUczniow = int.Parse(Console.ReadLine());
+
+                Uczen[] uczniowie = new Uczen[liczbaUczniow];
+
+                for (int i = 0; i < liczbaUczniow; i++)
+                {
+                    Console.WriteLine($"\nUczeń #{i + 1}");
+
+                    Console.Write("Imię: ");
+                    uczniowie[i].imie = Console.ReadLine();
+
+                    Console.Write("Nazwisko: ");
+                    uczniowie[i].nazwisko = Console.ReadLine();
+
+                    Console.Write("Ile ocen chcesz wprowadzić dla tego ucznia? ");
+                    int liczbaOcen = int.Parse(Console.ReadLine());
+
+                    uczniowie[i].oceny = new int[liczbaOcen];
+
+                    for (int j = 0; j < liczbaOcen; j++)
+                    {
+                        Console.Write($"Ocena {j + 1}: ");
+                        uczniowie[i].oceny[j] = int.Parse(Console.ReadLine());
+                    }
+                }
+
+                // Wyświetlenie danych
+                Console.WriteLine("\n--- Dane uczniów ---");
+
+                for (int i = 0; i < liczbaUczniow; i++)
+                {
+                    Console.WriteLine($"\nUczeń #{i + 1}: {uczniowie[i].imie} {uczniowie[i].nazwisko}");
+                    Console.Write("Oceny: ");
+                    foreach (int ocena in uczniowie[i].oceny)
+                    {
+                        Console.Write($"{ocena} ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
---
+++ rekurencjazad1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rekurencjazad1
+{
+    struct osoba
+    {
+        public string imie, nazwisko;
+        public int rok_urodzenia;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            osoba o;
+
+            Console.WriteLine("Podaj imie:");
+            o.imie = Console.ReadLine();
+
+            Console.WriteLine("Podaj nazwisko:");
+            o.nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Podaj rok urodzenia:");
+            o.rok_urodzenia = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Imie: {o.imie}, Nazwisko: {o.nazwisko}, Rok urodzenia: {o.rok_urodzenia}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
